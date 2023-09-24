bf73bd079062b408d82d9147e44c6f62e144ae8b
Helder Burato Berto <helder.burato@gmail.com>
2023-09-24 18:15:17 +0100

    using a var from package data

---
+++ go_io/data/constants.go
@@ -0,0 +1,3 @@
+package data
+
+const Url = "https://frontendmasters.com/go/io/data"
--- go_io/main.go
+++ go_io/main.go
@@ -1,7 +1,12 @@
 package main
 
+import "fmt"
+import "frontendmasters.com/go/io/data"
+
 var name string = "Helder"
 
 func main() {
+  fmt.Print(data.Url)
+
   PrintData()
 }
